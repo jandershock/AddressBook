def08d210510838672d5e3e382bd760a1049ced1
jandershock <jordan.andershock@gmail.com>
2022-05-25 15:22:02 -0500

    Finished address book from chapter 16

---
+++ AddressBook.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace AddressBookProject
+{
+    public class AddressBook
+    {
+        public Dictionary<string, Contact> Contacts { get; set; } = new Dictionary<string, Contact>();
+
+        public void AddContact(Contact contactArgument)
+        {
+            try
+            {
+                Contacts.Add(contactArgument.Email, contactArgument);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"{contactArgument.FullName} is already in contacts");
+            }
+        }
+
+        public Contact GetByEmail(string emailArgument)
+        {
+            return Contacts[emailArgument];
+        }
+    }
+}
